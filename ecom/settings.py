47c368beab8ea1f95ef45454b249e0f589e4c9f7
Yashwanth Raju <yyashwanth.raju.cse23@itbhu.ac.in>
2025-06-11 02:51:37 +0530

    added allowed hosts for railway

--- ecom/settings.py
+++ ecom/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-(brt8)_&f$p(%n2-j+5n8d4!=t2p4(dol#fw)(444qk9@m-28z
 DEBUG = True
 
 ALLOWED_HOSTS = ['plantpicks-production.up.railway.app','https://plantpicks-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['plantpicks-production.up.railway.app','https://plantpicks-production.up.railway.app'c]
+CSRF_TRUSTED_ORIGINS = ['https://plantpicks-production.up.railway.app']
 
 # Application definition
 
